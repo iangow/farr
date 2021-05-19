a62a5edc846092b5caf99446eb147bfed581e81a
Ian Gow <iandgow@gmail.com>
2021-05-19 07:06:37 -0400

    Tweaks to create_test_scores.R.
    - Made treatment consistent at individual level.
    - Ensured equal numbers of treatment and control observations.

--- data-raw/create_test_scores.R
+++ data-raw/create_test_scores.R
@@ -1,9 +1,9 @@
 library(dplyr, warn.conflicts = FALSE)
 set.seed(2021)
 
-f_assign <- function(x) {
+assign_score <- function(x) {
     temp <- 1 - (x - min(x))/(max(x) - min(x))
-    runif(n = length(x)) < temp
+    runif(n = length(x)) * temp
 }
 
 effect_size <- 15
@@ -12,13 +12,23 @@ talents <-
     tibble(id = 1:100L) %>%
     mutate(talent = rnorm(n = nrow(.), mean = 15, sd = 5))
 
-test_scores <-
+test_scores_pre <-
     expand.grid(grade = 8:11L, id = 1:100L) %>%
     inner_join(talents, by = "id") %>%
     mutate(score = rnorm(n = nrow(.), mean = 400, sd = 20) + talent +
-               grade * 10,
-           treat = f_assign(score),
-           post = grade >= 10,
+               grade * 10)
+
+treatment <-
+    test_scores_pre %>%
+    filter(grade == 9) %>%
+    mutate(treat_score = assign_score(score)) %>%
+    mutate(treat = treat_score > median(treat_score)) %>%
+    select(id, treat)
+
+test_scores <-
+    test_scores_pre %>%
+    inner_join(treatment, by = "id") %>%
+    mutate(post = grade >= 10,
            score = case_when(treat & post ~ score + effect_size,
                              TRUE ~ score)) %>%
     select(id, grade, post, treat, score) %>%
