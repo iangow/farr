95a99b69522c700030c48dac4d8e95f9569e5ecd
Ian Gow <iandgow@gmail.com>
2021-11-04 22:23:14 -0400

    Added winsorize and truncate functions.

---
+++ R/truncate.R
@@ -0,0 +1,18 @@
+#' Truncate a vector.
+#'
+#' Truncate a vector at prob and 1 - prob.
+#' Extreme values are turned in NA values.
+#'
+#' @param x A vector to be winsorized
+#' @param prob Level (two-sided) for winsorization (e.g., 0.01 gives 1% and 99%)
+#' @param p_low Optional lower level for winsorization (e.g., 0.01 gives 1%)
+#' @param p_high Optional upper level for winsorization (e.g., 0.99 gives 99%)
+#'
+#' @return vector
+#' @export
+truncate <- function(x, prob = 0.01, p_low = prob, p_high = 1 - prob) {
+    cuts <- stats::quantile(x, probs = c(p_low, p_high), type = 2, na.rm = TRUE)
+    x[x < cuts[1]] <- NA
+    x[x > cuts[2]] <- NA
+    x
+}
---
+++ R/winsorize.R
@@ -0,0 +1,17 @@
+#' Winsorize a vector.
+#'
+#' Winsorize a vector at prob and 1 - prob.
+#'
+#' @param x A vector to be winsorized
+#' @param prob Level (two-sided) for winsorization (e.g., 0.01 gives 1% and 99%)
+#' @param p_low Optional lower level for winsorization (e.g., 0.01 gives 1%)
+#' @param p_high Optional upper level for winsorization (e.g., 0.99 gives 99%)
+#'
+#' @return vector
+#' @export
+winsorize <- function(x, prob = 0.01, p_low = prob, p_high = 1 - prob) {
+    cuts <- stats::quantile(x, probs = c(p_low, p_high), type = 2, na.rm = TRUE)
+    x[x < cuts[1]] <- cuts[1]
+    x[x > cuts[2]] <- cuts[2]
+    x
+}
