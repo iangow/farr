ae86a64587e63106bf04e0e43e0bb97511ac7145
Ian Gow <iandgow@gmail.com>
2022-10-03 08:37:58 +1100

    Simplified create_aaer_firm_year.R.
    - Deleted data file.

--- data-raw/create_aaer_firm_year.R
+++ data-raw/create_aaer_firm_year.R
@@ -4,19 +4,18 @@ library(stringr)
 library(tidyr)
 
 jar_data <-
-    read_csv("data-raw/data_FraudDetection_JAR2020.csv.gz") %>%
+    read_csv(paste0("https://raw.githubusercontent.com/JarFraud/",
+                    "FraudDetection/master/",
+                    "data_FraudDetection_JAR2020.csv"),
+             col_types = "d") %>%
     mutate(gvkey = str_pad(gvkey, 6, side = "left", pad = "0"),
            fyear = as.integer(fyear),
            p_aaer = as.character(p_aaer))
 
-jar_frauds <-
+aaer_firm_year <-
     jar_data %>%
     filter(!is.na(p_aaer)) %>%
-    select(gvkey, fyear, p_aaer, new_p_aaer, understatement) %>%
-    mutate(understatement = as.logical(understatement))
-
-aaer_firm_year <-
-    jar_frauds %>%
+    select(gvkey, fyear, p_aaer) %>%
     group_by(p_aaer, gvkey) %>%
     summarize(min_year = min(fyear), max_year = max(fyear), .groups = 'drop')
 
