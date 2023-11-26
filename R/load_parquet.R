7be6ed022b6c6c931fa11cbb1bc2bc85f012c9a5
Ian Gow <iandgow@gmail.com>
2023-11-26 12:35:42 -0500

    Switched order of arguments for load_parquet().

--- R/load_parquet.R
+++ R/load_parquet.R
@@ -4,13 +4,13 @@
 #' into a table in the DuckDB database at conn.
 #'
 #' @param conn DuckDB connection
-#' @param schema Database schema for table
 #' @param table_name Name of table to be loaded
+#' @param schema Database schema for table
 #' @param data_dir Directory for data repository
 #'
 #' @return Remote data frame in conn
 #' @export
-load_parquet <- function(conn, schema, table_name,
+load_parquet <- function(conn, table_name, schema = "",
                          data_dir = Sys.getenv("DATA_DIR")) {
     file_path <- file.path(data_dir, schema,
                            paste0(table_name, ".parquet"))
