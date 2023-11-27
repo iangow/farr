817a320bf4c514da931dc27a78cfa590a325c693
Ian Gow <iandgow@gmail.com>
2023-11-26 21:33:14 -0500

    Tweaks to load_parquet().

--- R/load_parquet.R
+++ R/load_parquet.R
@@ -4,16 +4,16 @@
 #' into a table in the DuckDB database at conn.
 #'
 #' @param conn DuckDB connection
-#' @param table_name Name of table to be loaded
+#' @param table Name of table to be loaded
 #' @param schema Database schema for table
 #' @param data_dir Directory for data repository
 #'
 #' @return Remote data frame in conn
 #' @export
-load_parquet <- function(conn, table_name, schema = "",
+load_parquet <- function(conn, table, schema = "",
                          data_dir = Sys.getenv("DATA_DIR")) {
     file_path <- file.path(data_dir, schema,
-                           paste0(table_name, ".parquet"))
+                           paste0(table, ".parquet"))
     df_sql <- paste0("SELECT * FROM read_parquet('", file_path, "')")
     dplyr::tbl(conn, dplyr::sql(df_sql))
 }
