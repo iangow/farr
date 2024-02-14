bd765efee15aab94d41fa77470a045bbf0c15eb5
Ian Gow <iandgow@gmail.com>
2024-02-14 11:34:39 -0500

    Added system_time().

---
+++ R/system_time.R
@@ -0,0 +1,12 @@
+#' Version of `system.time()` that works with assignment
+#'
+#' Print CPU (and other) times that `expr` used, return value of `expr`
+#'
+#' @param expr Valid R expression to be timed, evaluated and returned
+#'
+#' @return Result of evaluating `expr`
+#' @export
+system_time <- function(expr) {
+    print(system.time(expr))
+    expr
+}
